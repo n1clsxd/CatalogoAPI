f0500c98739cd03f1ac9cbc547c140e02afb1e97
n1clsxd <nicolasmickus@gmail.com>
2023-07-18 11:10:29 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CatalogoAPI.Context;
+public class AppDbContext : DbContext
+{
+    //
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+
+
+}
---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogoAPI.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? ImageURL { get; set; }
+
+}
